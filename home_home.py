d94963c2d47f69f90b266a6bfdd20edaaaeb5013
Arya0443 <as3826@g.rit.edu>
2021-11-25 22:14:43 +0400

    Created home_home and test modules. Added class home and home_category in home_home module. Defined main function and created dictionaries under it.

---
+++ home_home.py
@@ -0,0 +1,20 @@
+class home:
+    pass
+
+class home_category:
+    __slots__ = ["garden", "indoor living space", "bathroom"]
+
+def main():
+    dict_garden1 = {"F": "Flowers", 12}
+    dict_garden2 = {"B": "Bench",30}
+    dict_garden3 = {"U": "Umbrella",15}
+    dict_garden4 = {"W": "Water_hose",20}
+    dict_indoor1 = {"T": "Table", 35}
+    dict_indoor2 = {"C": "Chair", 25}
+    dict_indoor3 = {"S": "Sofa", 60}
+    dict_indoor4 = {"R": "Shoe Rack", 50}
+    dict_bathroom1 = {"H": "Handwash", 8}
+    dict_bathroom2 = {"M": "Mat", 10}
+    dict_bathroom3 = {"Tb": "Toothbrush", 5}
+    dict_bathroom4 = {"Tl": "Towel", 18}
+
