f9c47f4213ab94bf96825846afb4062496abd7cf
Arya0443 <as3826@g.rit.edu>
2021-11-28 21:41:20 +0400

    Added dictionaries with prices of items and starting price as $50 as mentioned in sample run.

--- home_home.py
+++ home_home.py
@@ -13,3 +13,8 @@ class Home_Category:
         self.code = code
         self.price = price
         self.category = category
+
+home = Home([],[],[],50.0)
+garden_items = {"p": ("3 Pack garden flower", 5.0), "l": ("Hanging light wire", 10.0), "b": ("Garden bench", 35.0)}
+indoor_items = {"t": ("Small table lamp", 5.0), "f1": ("City picture frame", 7.0), "r": ("Entry rug", 35.0), "f2": ("Flower vase", 14.0)}
+bathroom_items = {"m": ("Wall mirror", 15.0), "c": ("Shower curtains", 10.0), "w": ("Wall mount", 7.0)}
